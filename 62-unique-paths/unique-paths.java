6309d8449cd573fbc43d6bf3c5df2655f72f683e
krishna-192006 <kp985203@gmail.com>
2026-08-08 18:20:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (48.47%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,29 +1,71 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
+    public int uniquePaths(int m, int n){
+        // Tabulation
         int dp[][] = new int[m][n];
-        for(int row[]:dp) {
-            Arrays.fill(row,-1);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i ==0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+        int up = 0, left = 0;
+                if(i>0){
+                    up = dp[i-1][j];
+                } 
+
+                if(j > 0) {
+                    left = dp[i][j-1];
+                }
+                dp[i][j] = up + left;
+            }
         }
-        return helper(m-1,n-1,dp);
-    }
-    int helper(int i, int j,int dp[][]){
-
-        if(i == 0 && j == 0) {
-            return 1;
-        }
-
-        if(i < 0 || j < 0) {
-            return 0;
-        }
-
-        if(dp[i][j] != -1)  {
-            return dp[i][j];
-        }
-
-        int up = helper(i-1,j,dp);
-        int left = helper(i,j-1,dp);
-        dp[i][j] = up + left;
-
-        return dp[i][j];
+        return dp[m-1][n-1];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Memoization
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int dp[][] = new int[m][n];
+//         for(int row[]:dp) {
+//             Arrays.fill(row,-1);
+//         }
+//         return helper(m-1,n-1,dp);
+//     }
+//     int helper(int i, int j,int dp[][]){
+
+//         if(i == 0 && j == 0) {
+//             return 1;
+//         }
+
+//         if(i < 0 || j < 0) {
+//             return 0;
+//         }
+
+//         if(dp[i][j] != -1)  {
+//             return dp[i][j];
+//         }
+
+//         int up = helper(i-1,j,dp);
+//         int left = helper(i,j-1,dp);
+//         dp[i][j] = up + left;
+
+//         return dp[i][j];
+//     }
+// }
