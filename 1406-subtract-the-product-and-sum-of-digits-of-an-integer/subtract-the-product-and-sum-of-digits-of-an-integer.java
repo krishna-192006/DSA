78c5c189dd409ed2d7d9723e1c9c334de4eadbca
krishna-192006 <kp985203@gmail.com>
2026-08-10 01:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.44%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        while(n > 0) {
+            int temp = n;
+            temp = temp%10;
+            product = product * temp;
+            sum = sum + temp;
+            n = n/10;
+        }
+        return product - sum;
+    }
+}
