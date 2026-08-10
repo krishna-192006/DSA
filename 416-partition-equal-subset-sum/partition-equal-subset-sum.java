6c227642eddf7539c1f0663149e2ca06c30838e8
krishna-192006 <kp985203@gmail.com>
2026-08-11 03:06:46 +0530

    Time: 50 ms (47.50%) | Memory: 47.5 MB (53.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        // TABULATION
         int n = nums.length;
         int total = 0;
         for(int num:nums){
@@ -10,23 +11,42 @@ class Solution {
         if(total % 2 != 0) {
             return false;
         }
-        Boolean dp[][] = new Boolean[n][target+1];
-        return helper(n-1,target,nums,dp);
-    }
+        boolean dp[][] = new boolean[n][target+1];
 
-    boolean helper(int i,int target,int arr[],Boolean dp[][]){
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int t=1;t<=target;t++){
+                boolean notTake = dp[i-1][t];
+                boolean take = false;
+                if(t >= nums[i]) {
+                    take = dp[i-1][t-nums[i]];
+                }
+                dp[i][t] = take || notTake;
+            }
+        }
+        return dp[n-1][target];
+    }
+    // MEMOIZATION
+    // boolean helper(int i,int target,int arr[],Boolean dp[][]){
         
-        if(dp[i][target] != null) {
-            return dp[i][target];
-        }
-        if(target == 0) return true;
-        if(i == 0) return (arr[0] == target);
+    //     if(dp[i][target] != null) {
+    //         return dp[i][target];
+    //     }
+    //     if(target == 0) return true;
+    //     if(i == 0) return (arr[0] == target);
 
-        boolean take = helper(i-1,target,arr,dp);
-        boolean notTake = false;
-        if(target >= arr[i]){
-            notTake = helper(i-1,target-arr[i],arr,dp);
-        }
-        return dp[i][target] =  (take || notTake);
-    }
+    //     boolean take = helper(i-1,target,arr,dp);
+    //     boolean notTake = false;
+    //     if(target >= arr[i]){
+    //         notTake = helper(i-1,target-arr[i],arr,dp);
+    //     }
+    //     return dp[i][target] =  (take || notTake);
+    // }
 }
