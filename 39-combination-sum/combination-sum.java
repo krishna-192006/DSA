b884000596617d7a19164453bc9d0fbfe042d35d
krishna-192006 <kp985203@gmail.com>
2026-08-10 06:01:05 +0530

    Time: 2 ms (96.25%) | Memory: 45.8 MB (33.65%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,28 +1,24 @@
 class Solution {
-    void Combination(int idx, int arr[],int sum, int target,List<Integer> li, List<List<Integer>> ans) {
-        if(idx == arr.length) {
-            if(sum == target) {
-               ans.add(new ArrayList<>(li));
-            }
-        return;
-        }
-
-        if(sum + arr[idx] <= target) {
-        li.add(arr[idx]);
-        // sum += arr[idx];
-        Combination(idx,arr,sum + arr[idx],target,li,ans);
-        li.remove(li.size()-1);
-        }
-
-        
-        // sum -= arr[idx];
-        Combination(idx+1,arr,sum,target,li,ans);
-
-    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        Combination(0,candidates,0,target,new ArrayList<>(),ans);
+        List<Integer> li = new ArrayList<>();
+        Combination(0,candidates,target,ans,0,li);
         return ans;
+    }
+    void Combination(int idx,int[] arr,int target,List<List<Integer>> ans,int sum, List<Integer> li) {
+        if(idx == arr.length) {
+        if(sum == target) {
+            ans.add(new ArrayList<>(li));
+        }
+    return;
+    }
+
+    if(sum + arr[idx] <= target) {
+        li.add(arr[idx]);
+        Combination(idx,arr,target,ans,sum+arr[idx],li);
+        li.remove(li.size() - 1);
+    }
+    Combination(idx+1,arr,target,ans,sum,li);
 
     }
 }
