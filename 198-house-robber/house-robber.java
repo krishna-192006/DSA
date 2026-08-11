e46e6b88376f4feb1799b7c3224671d964f71069
krishna-192006 <kp985203@gmail.com>
2026-08-12 00:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (88.43%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,40 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-    int n = nums.length;
-    int dp[] = new int[n];
-    dp[0] = nums[0];
-    for(int i=1;i<n;i++) {
-        int pick = nums[i];
-        if(i > 1) {
-            pick += dp[i-2];
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        return helper(0,nums,dp);
+    }
+    int helper(int i, int arr[],int dp[]) {
+        if(i >= arr.length){
+            return 0;
         }
-        int notPick = dp[i-1];
-        dp[i] = Math.max(pick,notPick);
-    }
-    return dp[n-1];
-       
 
-    }
-    // MEmoization
-    // int helper(int ind, int n, int arr[], int dp[]) {
-    //     if (ind >= n) {
-    //         return 0;
-    //     }
-    //     if (dp[ind] != -1) {
-    //         return dp[ind];
-    //     }
-    //     int pick = arr[ind] + helper(ind + 2, n, arr, dp);
-    //     int notpick = helper(ind + 1, n, arr, dp);
-    //     return dp[ind] = Math.max(pick,notpick);
+        if(dp[i] != -1){
+            return dp[i];
+        }
 
-    // }
-    //  REcursive Solution O(2^400) TLE
-    // int helper(int ind,int n, int arr[]) {
-    //     if(ind >= n) {
-    //         return 0;
-    //     }
-    //     int pick = arr[ind] + helper(ind+2,n,arr);
-    //     int notpick = helper(ind+1,n,arr);
-    //     return Math.max(pick,notpick);
-    // }
+        int take = arr[i] + helper(i+2,arr,dp);
+        int notTake = helper(i+1,arr,dp);
+
+        return dp[i] = Math.max(take,notTake);
+    }
 }
