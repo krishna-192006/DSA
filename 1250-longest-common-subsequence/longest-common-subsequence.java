68672335e85bd383cf747b50c89509a8e2926926
krishna-192006 <kp985203@gmail.com>
2026-08-20 18:39:31 +0530

    Time: 21 ms (54.35%) | Memory: 54 MB (52.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,27 +5,43 @@ class Solution {
         // return helper(m-1,n-1,text1,text2); 
         int m = text1.length();
         int n = text2.length();
-        int dp[][] = new int[m][n];
-        for(int row[] : dp) {
-            Arrays.fill(row,-1);
+        int dp[][] = new int[m+1][n+1];
+
+        for(int j=0;j<=n;j++) {
+            dp[0][j] = 0;
         }
-        return helper(m-1,n-1,text1,text2,dp); 
+
+        for(int i=0;i<=m;i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+         
     }
     // Memoization
-     int helper(int ind1, int ind2, String s1, String s2,int dp[][]) {
-        if(ind1 < 0 || ind2 < 0) {
-            return 0;
-        }
+    //  int helper(int ind1, int ind2, String s1, String s2,int dp[][]) {
+    //     if(ind1 < 0 || ind2 < 0) {
+    //         return 0;
+    //     }
 
-        if(dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
-        }
+    //     if(dp[ind1][ind2] != -1) {
+    //         return dp[ind1][ind2];
+    //     }
 
-        if(s1.charAt(ind1) == s2.charAt(ind2)) {
-            return dp[ind1][ind2] = 1 + helper(ind1-1,ind2-1,s1,s2,dp);
-        } else {
-           return dp[ind1][ind2] = Math.max(helper(ind1-1,ind2,s1,s2,dp),helper(ind1,ind2-1,s1,s2,dp));
-        }
+    //     if(s1.charAt(ind1) == s2.charAt(ind2)) {
+    //         return dp[ind1][ind2] = 1 + helper(ind1-1,ind2-1,s1,s2,dp);
+    //     } else {
+    //        return dp[ind1][ind2] = Math.max(helper(ind1-1,ind2,s1,s2,dp),helper(ind1,ind2-1,s1,s2,dp));
+    //     }
     }
     // Brute Force
     // int helper(int ind1, int ind2, String s1, String s2) {
@@ -39,4 +55,3 @@ class Solution {
     //         return Math.max(helper(ind1-1,ind2,s1,s2),helper(ind1,ind2-1,s1,s2));
     //     }
     // }
-}
